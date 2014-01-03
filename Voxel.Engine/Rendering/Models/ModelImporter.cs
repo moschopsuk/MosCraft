2c5b39d530972d2762b1b1569cbea095078d1971
Luke Moscrop <l.moscrop@lancaster.ac.uk>
2014-01-03 16:52:33 +0000

    Started .obj loader

---
+++ Voxel.Engine/Rendering/Models/ModelImporter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Voxel.Engine.Rendering.Models
+{
+    class ModelImporter
+    {
+        static public Model LoadOBJ(string fileName)
+        {
+            var reader = new StreamReader(fileName);
+            string line;
+            char[] splitChars = { ' ' };
+            while ((line = reader.ReadLine()) != null)
+            {
+                line = line.Trim(splitChars);
+                line = line.Replace("  ", " ");
+                string[] parameters = line.Split(splitChars);
+
+                switch (parameters[0])
+                {
+                }
+            }
+
+        }
+    }
+}
